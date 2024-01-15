72c0ef1b4f14ffb51b3d7bc7bf31b96ef8fe888b
JeffKim <younggonkim@Jeff-MacBook-Pro.local>
2024-01-15 22:39:54 +0900

    daily commit

--- src/main/scala/c/x/horus/sample/streaming/DbZStream.scala
+++ src/main/scala/c/x/horus/sample/streaming/DbZStream.scala
@@ -6,10 +6,23 @@ import zio.*
 import zio.stream.*
 
 object DbZStream extends ZIOAppDefault {
-  val users: ZStream[StockItemRepo, Throwable, StockItem] =
+  val stockStream: ZStream[StockItemRepo, Throwable, StockItem] =
     ZStream.fromIterableZIO(StockItemRepo.getAll)
 
-  val application = users.foreach(a => Console.printLine(a))
+//  val application = users.foreach(a => Console.printLine(a))
+
+//  val criFilter: String => Boolean = stockName =>
+//    stockName.contains("크리")
+
+  val criFilterPipeline: ZPipeline[Any, Nothing, StockItem, StockItem] =
+    ZPipeline.filter(si => si.companyNm.contains("크리"))
+
+//  val printlnZink: ZSink[Any, Throwable, ]
+  import Console._
+
+  val application = stockStream
+    .via(criFilterPipeline)
+    .run(ZSink.foreach(si => Console.printLine(si)))
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = 
     application.provide(
---
+++ src/main/scala/c/x/horus/sample/streaming/NetworkStream.scala
@@ -0,0 +1,5 @@
+package c.x.horus.sample.streaming
+
+class NetworkStream {
+
+}
